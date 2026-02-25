3c30b55364238658d0cf742ebaf5c1fd256e8384
rita0128 <98005429+rita0128@users.noreply.github.com>
2026-02-24 22:35:46 -0800

    Refactor Processor module and clean up code

--- Processor.v
+++ Processor.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module processor(
+module Processor(
     input clk,
     input rst
 );
@@ -11,110 +11,108 @@ wire [1: 0] forwardA, forwardB; // forwording
 wire forwardC; // hazard forwarding
 
 // IF stage
-wire [31: 0] if_pc; // instrction
-wire [31: 0] if_pc4; // pc + 4
-wire [31: 0] if_nextPc; // next instruction
-wire [31: 0] if_instr; 
+wire [31: 0] PC_IF; 
+wire [31: 0] PC4_IF; // pc + 4
+wire [31: 0] NEXTPC_IF; 
+wire [31: 0] INST_IF; 
 
 // ID stage
-wire [31: 0] id_pc;
-wire [31: 0] id_instr;
-wire [31: 0] id_imm32; // 32bit immediate number
-wire [31: 0] id_rs1Data, id_rs2Data;
-wire [4:  0] id_rd, id_rs1, id_rs2;
+wire [31: 0] PC_ID;
+wire [31: 0] INST_ID;
+wire [31: 0] IMM32_ID;
+wire [31: 0] rs1Data_ID, rs2Data_ID;
+wire [4:  0] RD_ID, RS1_ID, RS2_ID;
 
-wire [6: 0] id_opcode; // opcode
-wire [2: 0] id_func3; 
-wire [6: 0] id_func7;
+wire [6: 0] Opcode_ID; 
+wire [2: 0] func3_ID; 
+wire [6: 0] func7_ID;
 
-wire [4:  0] id_aluc; // aluctrl
-wire id_aluOut_WB_memOut; //mux2
-wire id_rs1Data_EX_PC; //mux2
-wire[1: 0] id_rs2Data_EX_imm32_4; //mux3
-wire id_writeReg;
-wire [1: 0] id_writeMem; 
-wire [2: 0] id_readMem;
-wire [2: 0] id_extOP;
-wire [1: 0] id_pcImm_NEXTPC_rs1Imm;
+wire [4:  0] ALU_OP_ID; 
+wire RF_SRC_ID; 
+wire ALU_A_SRC_ID; 
+wire[1: 0] ALU_B_SRC_ID; //mux3 
+wire REG_WE_ID;
+wire [1: 0] MEM_WE_ID; 
+wire [2: 0] MEM_RE_ID;
+wire [2: 0] IMM_TYPE_ID;
+wire [1: 0] PC_SRC_ID;
 
 // EX stage
-wire [4:  0] ex_aluc; 
-wire ex_aluOut_WB_memOut; 
-wire ex_rs1Data_EX_PC;
-wire[1: 0] ex_rs2Data_EX_imm32_4;
-wire ex_writeReg;
-wire [1: 0] ex_writeMem;
-wire [2: 0] ex_readMem;
-wire[1: 0] ex_pcImm_NEXTPC_rs1Imm;
-wire [31: 0] ex_pc; 
-wire [31: 0] ex_rs1Data, ex_rs2Data;
-wire [31: 0] ex_true_rs1Data, ex_true_rs2Data;
-wire [31: 0] ex_imm32;
-wire [4: 0] ex_rd, ex_rs1, ex_rs2;
-wire [31: 0] ex_inAluA, ex_inAluB;
-wire [31: 0] ex_pcImm, ex_rs1Imm;
-wire [31: 0] ex_outAlu;
-wire ex_conditionBranch;
+wire [4:  0] ALU_OP_EX; 
+wire RF_SRC_EX; 
+wire ALU_A_SRC_EX;
+wire[1: 0] ALU_B_SRC_EX;
+wire REG_WE_EX; 
+wire [1: 0] MEM_WE_EX; 
+wire [2: 0] MEM_RE_EX;
+wire[1: 0] PC_SRC_EX; 
+wire [31: 0] PC_EX; 
+wire [31: 0] rs1Data_EX, rs2Data_EX;
+wire [31: 0] TRUE_RS1_EX, TRUE_RS2_EX; 
+wire [31: 0] IMM32_EX;
+wire [4: 0] RD_EX, RS1_EX, RS2_EX;
+wire [31: 0] ALU_IN_A_EX, ALU_IN_B_EX; 
+wire [31: 0] PC_IMM_EX, RS1_IMM_EX; 
+wire [31: 0] ALU_OUT_EX; 
+wire BRANCH_EX; 
 
-// ME stage
-wire me_aluOut_WB_memOut; 
-wire me_writeReg; 
-wire [1: 0] me_writeMem;
-wire [2: 0] me_readMem;
-wire[1: 0] me_pcImm_NEXTPC_rs1Imm;
-wire me_conditionBranch;
-wire [31: 0] me_pcImm, me_rs1Imm;
-wire [31: 0] me_outAlu;
-wire [31: 0] me_rs2Data;
-wire [4: 0] me_rd;
-wire [4: 0] me_rs2;
-wire [31: 0] me_true_rs2Data;
-wire [31: 0] me_outMem;
+// MEM stage
+wire RF_SRC_MEM;  
+wire REG_WE_MEM; 
+wire [1: 0] MEM_WE_MEM; 
+wire [2: 0] MEM_RE_MEM; 
+wire[1: 0] PC_SRC_MEM; 
+wire BRANCH_MEM; 
+wire [31: 0] PC_IMM_MEM, RS1_IMM_MEM; 
+wire [31: 0] ALU_OUT_MEM; 
+wire [31: 0] rs2Data_MEM;
+wire [4: 0] RD_MEM;
+wire [4: 0] RS2_MEM;
+wire [31: 0] TRUE_RS2_MEM; 
+wire [31: 0] MEM_OUT_MEM; 
 
 // WB stage
-wire wb_aluOut_WB_memOut; 
-wire wb_writeReg;
-wire [31: 0] wb_outMem;
-wire [31: 0] wb_outAlu;
-wire [31: 0] wb_rdData;
-wire [4: 0] wb_rd;
+wire RF_SRC_WB; 
+wire REG_WE_WB; 
+wire [31: 0] OUT_MEM_WB; 
+wire [31: 0] OUT_ALU_WB; 
+wire [31: 0] RD_DATA_WB; 
+wire [4: 0] RD_WB; 
     // =============================
     // ==== COMPONENTS START =======
     // =============================
 
 
     NEXT_PC next_pc(
-        .jump_sel(me_pcImm_NEXTPC_rs1Imm),
-        .conditon_branch(me_conditionBranch),
-        .pc4(if_pc4),
-        .pcImm(me_rs1Imm),
-        .next_pc(if_nextPc),
+        .jump_sel(PC_SRC_MEM),
+        .condition_branch(BRANCH_MEM),
+        .pc4(PC4_IF),
+        .pcImm(PC_IMM_MEM),
+        .rs1Imm(RS1_IMM_MEM),
+        .next_pc(NEXT_PC_IF),
         .flush(flush)
     );
 
     PC_ADD_4  pc_add_4(
-    .pc(if_pc),
+    .pc(PC_IF),
 
-    .pc_4(if_pc4)
+    .pc_4(PC4_IF)
     );
 
     PC pc_unit(
         .clk(clk),
         .rst(rst),
-        .pause(stall),
-        .flush(flush),
-        .pc_next(if_nextPc),
-        .pc(if_pc)
+	.pause(1'b0),
+	.flush(flush | stall),
+       // .pause(stall),
+        //.flush(flush),
+        .pc_next(NEXT_PC_IF),
+        .pc(PC_IF)
     );
 
-    IMEM instruction_mem(
-        .addr(pc_if[10:2]),
-        .dout(inst_if)
-    );
-
-    Instruction_MEM INSTRUCTION_MEM(
-    .pc(if_pc),
-    .instruction(if_instr)
+    instruction_mem INSTRUCTION_MEM(
+    .pc(PC_IF),
+    .instruction(INST_IF)
     );
 
 
@@ -126,62 +124,63 @@ wire [4: 0] wb_rd;
     .pause(stall),
     .flush(flush),
 
-    .PC_IF(if_pc),
-    .Inst_IF(if_instr),
+    .PC_IF(PC_IF),
+    .Inst_IF(INST_IF),
 
-    .PC_ID(id_pc),
-    .Inst_ID(id_instr),
+    .PC_ID(PC_ID),
+    .Inst_ID(INST_ID),
 
-    .opcode(id_opcode),
-    .func3(id_func3),
-    .func7(id_func7),
-    .rd(id_rd),
-    .rs1(id_rs1),
-    .rs2(id_rs2)
+    .opcode(Opcode_ID),
+    .func3(func3_ID),
+    .func7(func7_ID),
+    .rd(RD_ID),
+    .rs1(RS1_ID),
+    .rs2(RS2_ID)
    );
 
    controller CONTROLLER(
-    .opcode(id_opcode),
-    .func3(id_func3),
-    .func7(id_func7),
+    .opcode(Opcode_ID),
+    .func3(func3_ID),
+    .func7(func7_ID),
 
-    .Alu_Op(id_aluc),
-    .rf_src(id_aluOut_WB_memOut),
-    .ALU_A(id_rs1Data_EX_PC),
-    .ALU_B(id_rs2Data_EX_imm32_4),
-    .reg_we(id_writeReg),
-    .mem_we(id_writeMem),
-    .mem_re(id_readMem),
-    .imm_type(id_extOP),
-    .pc_src(id_pcImm_NEXTPC_rs1Imm)
+    .ALU_Op(ALU_OP_ID),
+    .rf_src(RF_SRC_ID),
+    .ALU_A(ALU_A_SRC_ID),
+    .ALU_B(ALU_B_SRC_ID),
+    .reg_we(REG_WE_ID),
+    .mem_we(MEM_WE_ID),
+    .mem_re(MEM_RE_ID),
+    .imm_type(IMM_TYPE_ID),
+    .pc_src(PC_SRC_ID)
 );
 
     Reg_file REG_FILE(
         .rst(rst),
         .clk(clk),
-        .reg_we(wb_writeReg),
-        .rs1(id_rs1),
-        .rs2(id_rs2),
-        .rd(wb_rd),
-        .rd_data(wb_rdData),
+        .reg_we(REG_WE_WB),
+        .rs1(RS1_ID),
+        .rs2(RS2_ID),
+        .rd(RD_WB),
+        .rd_data(RD_DATA_WB),
 
-        .rs1_data(id_rs1Data),
-        .rs2_data(id_rs2Data)
+        .rs1_data(rs1Data_ID),
+        .rs2_data(rs2Data_ID)
     );
 
 
     // -------- Immediate --------
     imm imm_gen(
-        .instr(inst_id),
-        .extOP(3'b000),
-        .imm_32(imm_id)
+        .instr(INST_ID),
+        .extOP(IMM_TYPE_ID),
+        //.extOP(3'b000),
+        .imm_32(IMM32_ID)
     );
 
    Hazard_Detection hazard_dection(
-    .ex_readMem(ex_readMem),
-    .ex_rd(ex_rd),
-    .id_rs1(id_rs1),
-    .id_rs2(id_rs2),
+    .ex_readMem(MEM_RE_EX),
+    .ex_rd(RD_EX),
+    .id_rs1(RS1_ID),
+    .id_rs2(RS2_ID),
 
     .stall(stall)
 );
@@ -193,100 +192,100 @@ ID_EX id_ex(
     .pause(stall),
     .flush(flush),
 
-    .ID_ALU_Op(id_aluc),
-    .ID_RF_SRC(id_aluOut_WB_memOut),
-    .ID_ALU_A(id_rs1Data_EX_PC),
-    .ID_ALU_B(id_rs2Data_EX_imm32_4),
-    .ID_REG_WE(id_writeReg),
-    .ID_MEM_WE(id_writeMem),
-    .ID_MEM_RE(id_readMem),
-    .ID_pc_src(id_pcImm_NEXTPC_rs1Imm),
-    .ID_PC(id_pc),
-    .ID_rs1_DATA(id_rs1Data),
-    .ID_rs2_DATA(id_rs2Data),
-    .ID_IMM32(id_imm32),
-    .ID_RD(id_rd),
-    .ID_rs1(id_rs1),
-    .ID_rs2(id_rs2),
+    .ID_ALU_Op(ALU_OP_ID),
+    .ID_RF_SRC(RF_SRC_ID),
+    .ID_ALU_A(ALU_A_SRC_ID),
+    .ID_ALU_B(ALU_B_SRC_ID),
+    .ID_REG_WE(REG_WE_ID),
+    .ID_MEM_WE(MEM_WE_ID),
+    .ID_MEM_RE(MEM_RE_ID),
+    .ID_pc_src(PC_SRC_ID),
+    .ID_PC(PC_ID),
+    .ID_rs1_DATA(rs1Data_ID),
+    .ID_rs2_DATA(rs2Data_ID),
+    .ID_IMM32(IMM32_ID),
+    .ID_RD(RD_ID),
+    .ID_rs1(RS1_ID),
+    .ID_rs2(RS2_ID),
 
-    .EX_ALU_Op(ex_aluc),
-    .EX_RF_SRC(ex_aluOut_WB_memOut),
-    .EX_ALU_A(ex_rs1Data_EX_PC),
-    .EX_ALU_B(ex_rs2Data_EX_imm32_4),
-    .EX_REG_WE(ex_writeReg),
-    .EX_MEM_WE(ex_writeMem),
-    .EX_MEM_RE(ex_readMem),
-    .EX_pc_src(ex_pcImm_NEXTPC_rs1Imm),
-    .EX_PC(ex_pc),
-    .EX_rs1_DATA(ex_rs1Data),
-    .EX_rs2_DATA(ex_rs2Data),
-    .EX_IMM32(ex_imm32),
-    .EX_RD(ex_rd),
-    .EX_rs1(ex_rs1),
-    .EX_rs2(ex_rs2)
+    .EX_ALU_Op(ALU_OP_EX),
+    .EX_RF_SRC(RF_SRC_EX),
+    .EX_ALU_A(ALU_A_SRC_EX),
+    .EX_ALU_B(ALU_B_SRC_EX),
+    .EX_REG_WE(REG_WE_EX),
+    .EX_MEM_WE(MEM_WE_EX),
+    .EX_MEM_RE(MEM_RE_EX),
+    .EX_pc_src(PC_SRC_EX),
+    .EX_PC(PC_EX),
+    .EX_rs1_DATA(rs1Data_EX),
+    .EX_rs2_DATA(rs2Data_EX),
+    .EX_IMM32(IMM32_EX),
+    .EX_RD(RD_EX),
+    .EX_rs1(RS1_EX),
+    .EX_rs2(RS2_EX)
 );
 
 MUX_3 MUX_FORWARD_A(
     .signal(forwardA),
-    .a(ex_rs1Data),
-    .b(me_outAlu),
-    .c(wb_rdData),
+    .a(rs1Data_EX),
+    .b(ALU_OUT_MEM),
+    .c(RD_DATA_WB),
 
-    .out(ex_true_rs1Data)
+    .out(TRUE_RS1_EX)
 );
 
 MUX_3 MUX_FORWARD_B(
     .signal(forwardB),
-    .a(ex_rs2Data),
-    .b(me_outAlu),
-    .c(wb_rdData),
+    .a(rs2Data_EX),
+    .b(ALU_OUT_MEM),
+    .c(RD_DATA_WB),
 
-    .out(ex_true_rs2Data)
+    .out(TRUE_RS2_EX)
 );
 
 MUX_2 MUX_EX_A(
-    .signal(ex_rs1Data_EX_PC),
-    .a(ex_true_rs1Data),
-    .b(ex_pc),
+    .signal(ALU_A_SRC_EX),
+    .a(TRUE_RS1_EX),
+    .b(PC_EX),
 
-    .out(ex_inAluA)
+    .out(ALU_IN_A_EX)
 );
 
 MUX_3 MUX_EX_B(
-    .signal(ex_rs2Data_EX_imm32_4),
-    .a(ex_true_rs2Data),
-    .b(ex_imm32),
+    .signal(ALU_B_SRC_EX),
+    .a(TRUE_RS2_EX),
+    .b(IMM32_EX),
     .c(32'd4),
 
-    .out(ex_inAluB)
+    .out(ALU_IN_B_EX)
 );
 
 PC_CAL pc_cal(
-    .pc(ex_pc),
-    .imm32(ex_imm32),
-    .rs1_data(ex_true_rs1Data),
+    .pc(PC_EX),
+    .imm32(IMM32_EX),
+    .rs1_data(TRUE_RS1_EX),
 
-    .pcImm(ex_pcImm),
-    .rs1Imm(ex_rs1Imm)
+    .pcImm(PC_IMM_EX),
+    .rs1Imm(RS1_IMM_EX)
 );
 
 ALU alu(
-    .ctrl(ex_aluc),
-    .a(ex_inAluA),
-    .b(ex_inAluB),
+    .ctrl(ALU_OP_EX),
+    .a(ALU_IN_A_EX),
+    .b(ALU_IN_B_EX),
 
-    .out(ex_outAlu),
-    .condition_branch(ex_conditionBranch)
+    .out(ALU_OUT_EX),
+    .condition_branch(BRANCH_EX)
 );
 
 Data_Forwarding FORWARDING(
-    .me_reg_we(me_writeReg),
-    .meM_rd(me_rd),
-    .wb_rd(wb_rd),
-    .wb_reg_we(wb_writeReg),
-    .ex_rs1(ex_rs1),
-    .ex_rs2(ex_rs2),
-    .mem_rs2(me_rs2),
+    .mem_reg_we(REG_WE_MEM),
+    .mem_rd(RD_MEM),
+    .wb_rd(RD_WB),
+    .wb_reg_we(REG_WE_WB),
+    .ex_rs1(RS1_EX),
+    .ex_rs2(RS2_EX),
+    .mem_rs2(RS2_MEM),
 
     .fwd_a_sel(forwardA),
     .fwd_b_sel(forwardB),
@@ -294,78 +293,77 @@ Data_Forwarding FORWARDING(
 );
 
 
-EX_ME ex_me(
+EX_MEM ex_mem(
     .clk(clk),
     .rst(rst),
     .flush(flush),
+    .EX_WB_SRC(RF_SRC_EX),
+    .EX_REG_WE(REG_WE_MEM),
+    .EX_MEM_WE(MEM_WE_EX),
+    .EX_MEM_RE(MEM_RE_EX),
+    .EX_PC_SRC(PC_SRC_EX),
+    .EX_BRANCH(BRANCH_EX),
+    .EX_PC_IMM(PC_IMM_EX),
+    .EX_RS1_IMM(RS1_IMM_EX),
+    .EX_ALU_OUT(ALU_OUT_EX),
+    .EX_RS2_DATA(TRUE_RS2_EX),
+    .EX_RD(RD_EX),
+    .EX_RS2(RS2_EX),
 
-    .EX_WB_SRC(ex_aluOut_WB_memOut),
-    .EX_REG_WE(ex_writeReg),
-    .EX_MEM_WE(ex_writeMem),
-    .EX_MEM_RE(ex_readMem),
-    .EX_PC_SRC(ex_pcImm_NEXTPC_rs1Imm),
-    .EX_BRANCH(ex_conditionBranch),
-    .EX_PC_IMM(ex_pcImm),
-    .EX_RS1_IMM(ex_rs1Imm),
-    .EX_ALU_OUT(ex_outAlu),
-    .EX_RS2_DATA(ex_true_rs2Data),
-    .EX_RD(ex_rd),
-    .EX_RS2(ex_rs2),
-
-    .MEM_WB_SRC(me_aluOut_WB_memOut),
-    .MEM_REG_WE(me_writeReg),
-    .MEM_MEM_WE(me_writeMem),
-    .MEM_MEM_RE(me_readMem),
-    .MEM_PC_SRC(me_pcImm_NEXTPC_rs1Imm),
-    .MEM_BRANCH(me_conditionBranch),
-    .MEM_PC_IMM(me_pcImm),
-    .MEM_RS1_IMM(me_rs1Imm),
-    .MEM_ALU_OUT(me_outAlu),
-    .MEM_RS2_DATA(me_rs2Data),
-    .MEM_RD(me_rd),
-    .MEM_RS2(me_rs2)
+    .MEM_WB_SRC(RF_SRC_MEM),
+    .MEM_REG_WE(REG_WE_MEM),
+    .MEM_MEM_WE(MEM_WE_MEM),
+    .MEM_MEM_RE(MEM_RE_MEM),
+    .MEM_PC_SRC(PC_SRC_MEM),
+    .MEM_BRANCH(BRANCH_MEM),
+    .MEM_PC_IMM(PC_IMM_MEM),
+    .MEM_RS1_IMM(RS1_IMM_MEM),
+    .MEM_ALU_OUT(ALU_OUT_MEM),
+    .MEM_RS2_DATA(rs2Data_MEM),
+    .MEM_RD(RD_MEM),
+    .MEM_RS2(RS2_MEM)
 );
 
 MUX_2 MUX_WB_DATA(
     .signal(forwardC),
-    .a(me_rs2Data),
-    .b(wb_rdData),
+    .a(rs2Data_MEM),
+    .b(RD_DATA_WB),
 
-    .out(me_true_rs2Data)
+    .out(TRUE_RS2_MEM)
 );
 
 DATA_MEM data_mem(
     .clk(clk),
     .rst(rst),
-    .addr(me_outAlu),
-    .din(me_true_rs2Data),
-    .mem_we(me_writeMem),
-    .mem_re(me_readMem),
+    .addr(ALU_OUT_MEM),
+    .din(TRUE_RS2_MEM),
+    .mem_we(MEM_WE_MEM),
+    .mem_re(MEM_RE_MEM),
 
-    .dout(me_outMem)
+    .dout(MEM_OUT_MEM)
 );
 
 MEM_WB mem_wb(
     .clk(clk),
     .rst(rst),
-    .mem_to_reg(me_aluOut_WB_memOut),
-    .mem_to_reg(me_writeReg),
-    .mem_outMem(me_outMem),
-    .mem_outAlu(me_outAlu),
-    .mem_rd(me_rd),
+    .mem_to_reg(RF_SRC_MEM),
+    .mem_to_we(REG_WE_MEM),
+    .mem_outMem(MEM_OUT_MEM),
+    .mem_outAlu(ALU_OUT_MEM),
+    .mem_rd(RD_MEM),
 
-    .wb_to_reg(wb_aluOut_WB_memOut),
-    .wb_reg_we(wb_writeReg),
-    .wb_outMem(wb_outMem),
-    .wb_outAlu(wb_outAlu),
-    .wb_rd(wb_rd)
+    .wb_to_reg(RF_SRC_WB),
+    .wb_reg_we(REG_WE_WB),
+    .wb_outMem(OUT_MEM_WB),
+    .wb_outAlu(OUT_ALU_WB),
+    .wb_rd(RD_WB)
 );
 
 MUX_2 MUX_WB(
-    .signal(wb_aluOut_WB_memOut),
-    .a(wb_outAlu),
-    .b(wb_outMem),
+    .signal(RF_SRC_WB),
+    .a(OUT_ALU_WB),
+    .b(OUT_MEM_WB),
 
-    .out(wb_rdData)
+    .out(RD_DATA_WB)
 );
 endmodule
